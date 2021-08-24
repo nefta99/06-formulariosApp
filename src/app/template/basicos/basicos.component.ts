08a0b591e5d5e6cab372d24261de82334a1aa17d
nefta99 <israelneftali@gmail.com>
2021-08-24 16:55:52 -0500

    video: 235

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -19,11 +19,24 @@ export class BasicosComponent implements OnInit {
   // guardar( miFormulario : NgForm){
   guardar(){
     console.log(this.miFormulario);
+
+   
   }
 
   nombreValido() : boolean{
     return this.miFormulario?.controls.producto?.invalid
     && this.miFormulario?.controls.producto?.touched
+  }
+
+  precioValido() : boolean{
+    
+  
+    return this.miFormulario?.controls.precio?.touched && 
+           this.miFormulario?.controls.precio?.value < 0;
+    
+    
+ 
+    
   }
 
 }
