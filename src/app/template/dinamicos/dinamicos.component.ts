da06371a5e1d5e8d7fc77dd8b5680d057164eafb
nefta99 <israelneftali@gmail.com>
2021-08-25 10:29:41 -0500

    video : 238 tarea hecha por mi

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario! : NgForm;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hayErrorNombre() :  boolean{
+    let bandera : boolean = false;
+     
+     if(this.miFormulario?.controls.Nombre?.errors && this.miFormulario?.controls.Nombre?.touched ){
+       bandera =true;
+     }
+
+
+    return bandera;
+
+  }
+
+
+  guardar(){
+     //console.log(this.miFormulario);    
+
+
+     let bandera : boolean = false;
+     //  console.log(this.miFormulario.controls.Nombre.errors);
+     if(this.miFormulario!.controls.Nombre!.errors){
+       bandera =true;
+     }
+
+     console.log("Hay error en nombre "+bandera);
+
+  }
+
+
+  
 }
