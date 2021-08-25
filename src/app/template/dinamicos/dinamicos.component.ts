457f0d64bae0c642e064121c74d3ff166944b029
nefta99 <israelneftali@gmail.com>
2021-08-25 16:17:16 -0500

    video : 239

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
+interface Persona{
+  nombre:string ;
+  favoritos : Favorito[]
+}
+
+interface Favorito{
+  id:number;
+  nombre:string ;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
   @ViewChild('miFormulario') miFormulario! : NgForm;
 
-
-  constructor() { }
-
-  ngOnInit(): void {
+  persona : Persona = {
+    nombre:  'Israel',
+    favoritos:[
+      {
+        id: 1,
+        nombre:'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre:'Death Stranding'
+      },
+    ]
   }
+ 
 
   hayErrorNombre() :  boolean{
     let bandera : boolean = false;
