5b50ccb213b76472ac3c85897d06475124690e8e
neftec99 <neftalitorres@tec.mx>
2021-08-28 13:01:33 -0500

    video : 249+

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -10,11 +10,19 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class BasicosComponent {
 
 
-  miFormulario : FormGroup  = new FormGroup({
-    //Esto tiene que concordar en el html con la propiedad formControlName = "nombre"
-    'nombre' : new FormControl('RTX 4080ti')
+  // miFormulario : FormGroup  = new FormGroup({
+  //   //Esto tiene que concordar en el html con la propiedad formControlName = "nombre"
+  //   nombre      : new FormControl('RTX 4080ti'),
+  //   precio      : new FormControl(1500),
+  //   existencias : new FormControl(5),
+  // })
+
+
+  miFormulario : FormGroup =this.fb.group({
+    nombre: ['RTZs 4080ti']
   })
-  constructor() { }
+
+  constructor(private fb : FormBuilder) { }
 
   
 
