0ec5476467b4f074fdfe9629cc171419f9388348
neftec99 <neftalitorres@tec.mx>
2021-08-31 15:28:05 -0500

    Video : 257

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit  {
 
-  constructor() { }
+  miFormulario  :FormGroup = this.fb.group({
+      genero: ['M',Validators.required],
+      notificaciones: [false,Validators.required],
+      condiciones:[false,Validators.requiredTrue]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero : 'F',
+    notificaciones : true,
+
+  }
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit(){
+    //Esta iniciando los valores del formulario con este objeto de this.persona.
+    this.miFormulario.reset({
+      ...this.persona,
+      /*
+        Esto de condicion esta agregado dentro del objetoi miformulario
+        Pero  no dentro de persona entonces con este codigo dentro del oninit
+        logramos que se establesca el control de condiciones como en false para que el 
+        html lo marque como false y el miFormulario.valid este como false
+      */
+      condiciones : false 
+    });
   }
 
 }
